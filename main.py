1f45503c37029e8136f0285eb0f50438e14844e5
firebirdhq <137559531+firebirdhq@users.noreply.github.com>
2024-01-07 11:03:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to another beautiful day on earth.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
